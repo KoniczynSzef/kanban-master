384bcc70f267870f5b90c554ed4fdb6337bfcbda
KoniczynSzef <koniczynszef@gmail.com>
2024-04-12 08:54:43 +0200

    chore: added some trpc procedures to check if the work in server components

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,12 +1,12 @@
 import CreateUser from "@/components/auth/CreateUser";
 import { Button } from "@/components/ui/button";
-import { getUserByKindeId } from "@/server/auth/get-user-by-kinde-id";
 import {
     LogoutLink,
     getKindeServerSession,
 } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import React, { FC } from "react";
+import { trpc } from "../_trpc/client";
 
 interface Props {}
 
@@ -20,7 +20,14 @@ const page: FC<Props> = async () => {
         return redirect("/");
     }
 
-    const { data: user } = await getUserByKindeId(kindeUser.id);
+    /**
+     * ! This doesn't work in server components
+     */
+    const { data: user } = trpc.getUserByKindeId.useQuery(kindeUser.id);
+
+    console.log(user);
+
+    // await getUserByKindeId(kindeUser.id);
 
     if (user?.validated) {
         return redirect("/");

db5c629769b9f4e89b541386a5fd6bad91781ee1
Koniczyn <piotr12konczyk27@gmail.com>
2024-04-14 20:11:12 +0200

    test: added tests for checking login or register click

---
+++ tests/e2e/auth/index.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "@playwright/test";
+
+import "dotenv/config";
+
+test("Click login button", async ({ page }) => {
+    console.log("AUTH_TEST_EMAIL", process.env.AUTH_TEST_EMAIL);
+
+    if (!process.env.AUTH_TEST_EMAIL) {
+        throw new Error("AUTH_TEST_EMAIL is not set");
+    }
+
+    await page.goto("http://localhost:3000");
+    await page.click("text=Login");
+
+    expect(await page.title()).toBe("Sign in | kanmaster");
+});
+
+test("Click register button", async ({ page }) => {
+    await page.goto("http://localhost:3000");
+    await page.click("text=Register");
+
+    expect(await page.title()).toBe("Sign up | kanmaster");
+});

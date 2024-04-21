1323254cf6f19bfd05f162a31c75c3387fe17733
Koniczyn <piotr12konczyk27@gmail.com>
2024-04-21 21:55:57 +0200

    fix: added next js link component to NavMenuItem link

--- components/homepage/navbar/menu/NavMenuItem.tsx
+++ components/homepage/navbar/menu/NavMenuItem.tsx
@@ -2,6 +2,7 @@ import { type NavMenuItem as TNavMenuItem } from "@/types/nav-menu-item";
 import React, { FC } from "react";
 
 import * as NavigationMenu from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 interface Props {
     navItem: TNavMenuItem;
@@ -19,12 +20,14 @@ const NavMenuItem: FC<Props> = (props) => {
                         key={item.href}
                         className="flex flex-col items-start gap-1"
                     >
-                        <NavigationMenu.NavigationMenuLink
-                            href={item.href}
-                            className={NavigationMenu.navigationMenuTriggerStyle()}
-                        >
-                            {item.title}
-                        </NavigationMenu.NavigationMenuLink>
+                        <Link href={item.href}>
+                            <NavigationMenu.NavigationMenuLink
+                                href={item.href}
+                                className={NavigationMenu.navigationMenuTriggerStyle()}
+                            >
+                                {item.title}
+                            </NavigationMenu.NavigationMenuLink>
+                        </Link>
                         <p className="text-sm text-muted-foreground ml-4">
                             {item.itemDescription}
                         </p>

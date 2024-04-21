842996db38008cbfb3cb6ea5195daa855fb82328
Koniczyn <piotr12konczyk27@gmail.com>
2024-04-21 21:52:16 +0200

    chore: created separated component for NavMenuItem and TS typing for them

---
+++ components/homepage/navbar/menu/NavMenuItem.tsx
@@ -0,0 +1,38 @@
+import { type NavMenuItem as TNavMenuItem } from "@/types/nav-menu-item";
+import React, { FC } from "react";
+
+import * as NavigationMenu from "@/components/ui/navigation-menu";
+
+interface Props {
+    navItem: TNavMenuItem;
+}
+
+const NavMenuItem: FC<Props> = (props) => {
+    return (
+        <NavigationMenu.NavigationMenuItem>
+            <NavigationMenu.NavigationMenuTrigger>
+                {props.navItem.triggerTitle}
+            </NavigationMenu.NavigationMenuTrigger>
+            <NavigationMenu.NavigationMenuContent className="p-4 py-6 grid grid-cols-2 md:w-[600px] gap-8">
+                {props.navItem.content.map((item) => (
+                    <div
+                        key={item.href}
+                        className="flex flex-col items-start gap-1"
+                    >
+                        <NavigationMenu.NavigationMenuLink
+                            href={item.href}
+                            className={NavigationMenu.navigationMenuTriggerStyle()}
+                        >
+                            {item.title}
+                        </NavigationMenu.NavigationMenuLink>
+                        <p className="text-sm text-muted-foreground ml-4">
+                            {item.itemDescription}
+                        </p>
+                    </div>
+                ))}
+            </NavigationMenu.NavigationMenuContent>
+        </NavigationMenu.NavigationMenuItem>
+    );
+};
+
+export default NavMenuItem;
--- components/homepage/navbar/menu/NavigationMenuWrapper.tsx
+++ components/homepage/navbar/menu/NavigationMenuWrapper.tsx
@@ -1,112 +1,70 @@
 "use client";
 
 import * as NavigationMenu from "@/components/ui/navigation-menu";
-import Link from "next/link";
+import { type NavMenuItem as TNavMenuItem } from "@/types/nav-menu-item";
 import React, { FC } from "react";
+import NavMenuItem from "./NavMenuItem";
 
 interface Props {}
+
+const navMenuItems: TNavMenuItem[] = [
+    {
+        triggerTitle: "Learn",
+        content: [
+            {
+                href: "/getting-started",
+                title: "Getting Started",
+                itemDescription:
+                    "Learn how to get started with KanMaster. Create your first team and project 🧑‍💻",
+            },
+            {
+                href: "/features",
+                title: "Features",
+                itemDescription:
+                    "Explore all the features that KanMaster offers. Learn how to manage your projects effortlessly 📊",
+            },
+            {
+                href: "/pricing",
+                title: "Pricing",
+                itemDescription:
+                    "Check out our pricing plans and choose the one that fits your needs. Start for free 🎁",
+            },
+            {
+                href: "/integrations",
+                title: "Integrations",
+                itemDescription:
+                    "Connect your favorite tools with KanMaster. Get more done with less effort 🛠️",
+            },
+        ],
+    },
+    {
+        triggerTitle: "Manage",
+        content: [
+            {
+                href: "/projects",
+                title: "Dashboard",
+                itemDescription:
+                    "View all projects, tasks, and members in one place. Full power of Kanban is here 🚀",
+            },
+            {
+                href: "/projects/new",
+                title: "New Project",
+                itemDescription:
+                    "Create a new project and invite your team members. Start working together and get things done 🎉",
+            },
+        ],
+    },
+];
 
 const NavigationMenuWrapper: FC<Props> = () => {
     return (
         <NavigationMenu.NavigationMenu>
             <NavigationMenu.NavigationMenuList>
-                <NavigationMenu.NavigationMenuItem>
-                    <NavigationMenu.NavigationMenuTrigger>
-                        Learn
-                    </NavigationMenu.NavigationMenuTrigger>
-                    <NavigationMenu.NavigationMenuContent className="p-4 py-6 grid grid-cols-2 md:w-[600px] gap-8">
-                        <div className="flex flex-col items-start gap-1">
-                            <Link href="/features">
-                                <NavigationMenu.NavigationMenuLink
-                                    className={NavigationMenu.navigationMenuTriggerStyle()}
-                                >
-                                    Features
-                                </NavigationMenu.NavigationMenuLink>
-                            </Link>
-                            <p className="text-sm text-muted-foreground ml-4">
-                                Explore all the features that KanMaster offers.
-                                Learn how to manage your projects effortlessly
-                                📊
-                            </p>
-                        </div>
-                        <div className="flex flex-col items-start gap-1">
-                            <Link href="/pricing">
-                                <NavigationMenu.NavigationMenuLink
-                                    className={NavigationMenu.navigationMenuTriggerStyle()}
-                                >
-                                    Pricing
-                                </NavigationMenu.NavigationMenuLink>
-                            </Link>
-                            <p className="text-sm text-muted-foreground ml-4">
-                                Check out our pricing plans and choose the one
-                                that fits your needs. Start for free 🎁
-                            </p>
-                        </div>
-                        <div className="flex flex-col items-start gap-1">
-                            <Link href="/integrations">
-                                <NavigationMenu.NavigationMenuLink
-                                    className={NavigationMenu.navigationMenuTriggerStyle()}
-                                >
-                                    Integrations
-                                </NavigationMenu.NavigationMenuLink>
-                            </Link>
-                            <p className="text-sm text-muted-foreground ml-4">
-                                Connect your favorite tools with KanMaster. Get
-                                more done with less effort 🛠️
-                            </p>
-                        </div>
-                    </NavigationMenu.NavigationMenuContent>
-                </NavigationMenu.NavigationMenuItem>
-
-                <NavigationMenu.NavigationMenuItem>
-                    <NavigationMenu.NavigationMenuTrigger>
-                        Manage
-                    </NavigationMenu.NavigationMenuTrigger>
-                    <NavigationMenu.NavigationMenuContent className="p-4 py-6 grid grid-cols-2 md:w-[600px] gap-8">
-                        <div className="flex flex-col items-start gap-1">
-                            <Link href="/getting-started">
-                                <NavigationMenu.NavigationMenuLink
-                                    className={NavigationMenu.navigationMenuTriggerStyle()}
-                                >
-                                    Getting Started
-                                </NavigationMenu.NavigationMenuLink>
-                            </Link>
-                            <p className="text-sm text-muted-foreground ml-4">
-                                Learn how to get started with KanMaster. Create
-                                your first team and project 🧑‍💻
-                            </p>
-                        </div>
-                        <div className="flex flex-col items-start gap-1">
-                            <Link href="/projects">
-                                <NavigationMenu.NavigationMenuLink
-                                    className={`${NavigationMenu.navigationMenuTriggerStyle()}`}
-                                >
-                                    Dashboard
-                                </NavigationMenu.NavigationMenuLink>
-                            </Link>
-                            <p className="text-sm text-muted-foreground ml-4">
-                                View all projects, tasks, and members in one
-                                place. Full power of Kanban is here 🚀
-                            </p>
-                        </div>
-                        <div>
-                            <Link href="/projects/new">
-                                <NavigationMenu.NavigationMenuLink
-                                    className={NavigationMenu.navigationMenuTriggerStyle()}
-                                >
-                                    New Project
-                                </NavigationMenu.NavigationMenuLink>
-                            </Link>
-
-                            <p className="text-sm text-muted-foreground ml-4">
-                                Create a new project and invite your team
-                                members. Start working together and get things
-                                done 🎉
-                            </p>
-                        </div>
-                    </NavigationMenu.NavigationMenuContent>
-                </NavigationMenu.NavigationMenuItem>
+                {navMenuItems.map((navItem) => (
+                    <NavMenuItem key={navItem.triggerTitle} navItem={navItem} />
+                ))}
             </NavigationMenu.NavigationMenuList>
+            <NavigationMenu.NavigationMenuViewport />
         </NavigationMenu.NavigationMenu>
     );
 };

cf1ed6d3fd14a8fd3299b2f083e19dde074c597f
Koniczyn <piotr12konczyk27@gmail.com>
2024-04-25 20:54:00 +0200

    chore: decreased the width of navbar

--- components/homepage/navbar/Navbar.tsx
+++ components/homepage/navbar/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar: FC<Props> = async () => {
     const isUserLoggedIn = await isAuthenticated();
 
     return (
-        <header className="mt-4 py-4 left-4 right-4 md:left-16 md:right-16 lg:left-40 lg:right-40  border-purple-300 border-2 rounded-2xl fixed self-center z-50 backdrop-blur flex justify-around items-center">
+        <header className="mt-4 py-4 left-4 right-4 md:left-16 md:right-16 lg:left-36 lg:right-36 border-purple-300 border-2 rounded-2xl fixed self-center z-50 backdrop-blur flex justify-around items-center">
             <HomeLink />
 
             <NavigationMenu className="hidden md:block" />

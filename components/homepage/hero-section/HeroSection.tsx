1860256dd7b36148a3f1a4c3396a01140eb370f2
Koniczyn <piotr12konczyk27@gmail.com>
2024-04-29 21:26:20 +0200

    chore: created new image

--- components/homepage/hero-section/HeroSection.tsx
+++ components/homepage/hero-section/HeroSection.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from "react";
 import AnimatedHeader from "./AnimatedHeader";
 import AnimatedDescription from "./AnimatedDescription";
+import Image from "next/image";
+
+import kanbanSlogan from "@/images/kanban-slogan.svg";
 
 interface Props {}
 
 const HeroSection: FC<Props> = () => {
     return (
-        <div className="text-center flex flex-col items-center gap-4">
+        <div className="relative text-center flex flex-col items-center gap-4">
             <AnimatedHeader>
                 <h1 className="text-6xl font-extrabold uppercase">
                     Manage your workflow
@@ -14,6 +17,14 @@ const HeroSection: FC<Props> = () => {
             </AnimatedHeader>
 
             <AnimatedDescription />
+
+            <Image
+                src={kanbanSlogan}
+                alt="Kanban is coooool!"
+                width={300}
+                height={300}
+                className="absolute -bottom-16 md:-bottom-4 left-4 translate-y-1/2 -rotate-3"
+            />
         </div>
     );
 };

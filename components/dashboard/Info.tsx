4de4955b3a0915a680df74e0f94bb10410d6a857
Koniczyn <piotr12konczyk27@gmail.com>
2024-04-30 17:22:40 +0200

    chore: added guard clauses on dashboard info component

--- components/dashboard/Info.tsx
+++ components/dashboard/Info.tsx
@@ -3,19 +3,35 @@
 import { trpc } from "@/server/trpc";
 import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/types";
 import React, { FC } from "react";
+import { Button } from "../ui/button";
 
 interface Props {
     kindeUser: KindeUser;
 }
 
 const Info: FC<Props> = (props) => {
-    const { data } = trpc.getUserAndTeams.useQuery(props.kindeUser.id);
-
-    return (
-        <div>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
+    const { data, isLoading } = trpc.getUserAndTeams.useQuery(
+        props.kindeUser.id
     );
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!data) {
+        return <div>No data</div>;
+    }
+
+    if (data.teams.length === 0) {
+        return (
+            <div className="border border-muted flex flex-col p-8 rounded-2xl self-start gap-4">
+                Create a team to get started
+                <Button className="self-start">Create a team</Button>
+            </div>
+        );
+    }
+
+    return <pre>{JSON.stringify(data, null, 2)}</pre>;
 };
 
 export default Info;

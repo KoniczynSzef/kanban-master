41a8cab1b9eb1737e82a99640cf17367d6f34408
Koniczyn <piotr12konczyk27@gmail.com>
2024-05-14 17:42:04 +0200

    test: added unit tests for create-team.ts

--- tests/unit/server/routes/teams/create-team.test.ts
+++ tests/unit/server/routes/teams/create-team.test.ts
@@ -2,6 +2,9 @@ import { describe, test, expect } from "bun:test";
 
 import { createTeam } from "@/server/routes/teams/create-team";
 import { CreateTeamSchema } from "@/types/schemas/teams/create-team-schema";
+import { db } from "@/database";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 const dataToInsert: CreateTeamSchema = {
     description: "description",
@@ -12,8 +15,27 @@ const dataToInsert: CreateTeamSchema = {
 };
 
 describe("createTeam", () => {
-    test("should create a team successfully", async () => {
+    test("should return null if id user with that id is not found", async () => {
         const team = await createTeam("userId", dataToInsert);
         expect(team).toBeNull();
+    });
+
+    test("should return team object if user is found", async () => {
+        const user = await db.query.users.findFirst({
+            where: eq(users.id, process.env.TEST_USER_ID as string),
+        });
+
+        expect(user).not.toBeNull();
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        const team = await createTeam(user.id, dataToInsert);
+
+        expect(team).toHaveProperty("name");
+        expect(team).toHaveProperty("description");
+        expect(team).toHaveProperty("teamChatLink");
+        expect(team).toHaveProperty("teamColor");
     });
 });

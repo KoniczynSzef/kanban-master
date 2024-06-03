3d92a2f0269b199ef33622660ed5d94721aac852
Koniczyn <piotr12konczyk27@gmail.com>
2024-06-03 17:12:39 +0200

    style: removed border from navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar: FC<Props> = async () => {
     const user = await getKindeUser();
 
     return (
-        <header className="mt-4 py-4 left-4 right-4 md:left-16 md:right-16 lg:left-36 lg:right-36 border-purple-300 border-2 rounded-2xl fixed self-center z-50 backdrop-blur flex justify-around items-center">
+        <header className="py-4 flex justify-around items-center">
             <HomeLink />
 
             <NavigationMenu className="hidden md:block" />

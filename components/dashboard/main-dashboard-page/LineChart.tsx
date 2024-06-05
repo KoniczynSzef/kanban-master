5ef20e1bc29e11a06a9211eaf8f0eec4b33529e1
Koniczyn <piotr12konczyk27@gmail.com>
2024-06-05 08:55:07 +0200

    style: changed mock data and resized responsive recharts container

--- components/dashboard/main-dashboard-page/LineChart.tsx
+++ components/dashboard/main-dashboard-page/LineChart.tsx
@@ -13,59 +13,119 @@ interface Props {}
 
 const mockData = [
     {
-        name: "Page A",
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
+        name: "Jan",
+        projects: 4,
+        teams: 2,
+        productivity: 5.75,
     },
+
     {
-        name: "Page B",
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
+        name: "Feb",
+        projects: 3,
+        teams: 3,
+        productivity: 5.5,
     },
+
     {
-        name: "Page C",
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
+        name: "Mar",
+        projects: 5,
+        teams: 4,
+        productivity: 6.25,
     },
+
     {
-        name: "Page D",
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
+        name: "Apr",
+        projects: 6,
+        teams: 5,
+        productivity: 6.5,
     },
+
     {
-        name: "Page E",
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
+        name: "May",
+        projects: 8,
+        teams: 7,
+        productivity: 7.5,
     },
+
     {
-        name: "Page F",
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
+        name: "Jun",
+        projects: 7,
+        teams: 6,
+        productivity: 7,
     },
+
     {
-        name: "Page G",
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
+        name: "Jul",
+        projects: 9,
+        teams: 8,
+        productivity: 8,
+    },
+
+    {
+        name: "Aug",
+        projects: 10,
+        teams: 9,
+        productivity: 8.5,
+    },
+
+    {
+        name: "Sep",
+        projects: 11,
+        teams: 10,
+        productivity: 9,
+    },
+
+    {
+        name: "Oct",
+        projects: 12,
+        teams: 11,
+        productivity: 9.5,
+    },
+
+    {
+        name: "Nov",
+        projects: 13,
+        teams: 12,
+        productivity: 10,
+    },
+
+    {
+        name: "Dec",
+        projects: 14,
+        teams: 13,
+        productivity: 10.5,
     },
 ];
 
 export const LineChart: React.FC<Props> = () => {
     return (
-        <ResponsiveContainer height={300} width={"65%"}>
-            <Chart width={600} height={300} data={mockData}>
-                <Line type={"monotone"} dataKey={"uv"} stroke="#ff0000" />
-                <CartesianGrid stroke="#ddd" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-            </Chart>
-        </ResponsiveContainer>
+        <div className="border-2 border-muted rounded-2xl p-8">
+            <h3 className="text-xl font-semibold">
+                This year&apos;s statistics
+            </h3>
+            <ResponsiveContainer height={300} className={"my-8"}>
+                <Chart width={600} height={300} data={mockData}>
+                    <CartesianGrid stroke="#ddd" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Line
+                        type={"monotone"}
+                        dataKey={"projects"}
+                        stroke="#4f46e5"
+                    />
+                    <Line
+                        type={"monotone"}
+                        dataKey={"teams"}
+                        stroke="#059669"
+                    />
+                    <Line
+                        type={"monotone"}
+                        dataKey={"productivity"}
+                        stroke="#334155"
+                    />
+                </Chart>
+            </ResponsiveContainer>
+        </div>
     );
 };

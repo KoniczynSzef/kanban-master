b2ff69ec7c66bd647466fd287967ba34701a8a2f
Koniczyn <koniczynszef@gmail.com>
2024-06-13 22:51:42 +0200

    chore: changed variable name

--- lib/auth/is-authenticated.ts
+++ lib/auth/is-authenticated.ts
@@ -2,7 +2,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
 export async function isAuthenticated(req?: NextRequest) {
-    const { isAuthenticated } = getKindeServerSession(req);
+    const { isAuthenticated: isUserAuthenticated } = getKindeServerSession(req);
 
-    return await isAuthenticated();
+    return await isUserAuthenticated();
 }

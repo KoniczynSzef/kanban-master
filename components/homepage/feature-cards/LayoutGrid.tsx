b421bef075408f074f07b3195bbc06122ce190a7
Koniczyn <piotr12konczyk27@gmail.com>
2024-06-27 11:48:25 +0200

    style: added contribute badge

--- components/homepage/feature-cards/LayoutGrid.tsx
+++ components/homepage/feature-cards/LayoutGrid.tsx
@@ -1,36 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { linkStyle } from "@/lib/link-style";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
 import React, { FC } from "react";
 import FeaturesGrid from "./FeaturesGrid";
-import { Github } from "lucide-react";
-import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 interface Props {}
 
 const LayoutGrid: FC<Props> = () => {
     return (
         <section className="flex flex-col gap-32 items-center">
-            <div className="flex gap-8 relative">
-                <LoginLink className={cn(linkStyle, "flex w-min")}>
-                    <Button tabIndex={-1}>Get Started</Button>
-                </LoginLink>
-                <Link
-                    href="https://github.com/koniczynszef/kanban-master"
-                    className={cn(linkStyle, "flex w-min focus:ring-secondary")}
-                    target="_blank"
-                >
-                    <Button
-                        tabIndex={-1}
-                        className="flex items-center gap-1"
-                        variant={"outline"}
-                    >
-                        <Github /> GitHub
-                    </Button>
-                </Link>
-            </div>
-
             <FeaturesGrid />
         </section>
     );
--- components/homepage/hero-section/HeroSection.tsx
+++ components/homepage/hero-section/HeroSection.tsx
@@ -1,14 +1,50 @@
 import React, { FC } from "react";
 import HeroDescription from "./HeroDescription";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { linkStyle } from "@/lib/link-style";
+import { Github } from "lucide-react";
+import Link from "next/link";
 
 interface Props {}
 
 const HeroSection: FC<Props> = () => {
     return (
         <div className="relative text-center flex flex-col items-center gap-4">
+            <Link
+                href={
+                    "https://github.com/koniczynszef/kanban-master/contribute"
+                }
+                className={cn(
+                    linkStyle,
+                    "bg-secondary-foreground text-white px-2 py-1 rounded-full focus:ring-secondary-foreground"
+                )}
+            >
+                Feel free to contribute!
+            </Link>
             <h1 className="text-7xl font-bold">Manage your workflow</h1>
 
             <HeroDescription />
+
+            <div className="flex gap-8 relative mt-8">
+                <LoginLink className={cn(linkStyle, "flex w-min")}>
+                    <Button tabIndex={-1}>Get Started</Button>
+                </LoginLink>
+                <Link
+                    href="https://github.com/koniczynszef/kanban-master"
+                    className={cn(linkStyle, "flex w-min focus:ring-secondary")}
+                    target="_blank"
+                >
+                    <Button
+                        tabIndex={-1}
+                        className="flex items-center gap-1"
+                        variant={"outline"}
+                    >
+                        <Github /> GitHub
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 };

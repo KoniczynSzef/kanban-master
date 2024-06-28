a95facded78312dc55097c02d83a552f306303d7
Koniczyn <piotr12konczyk27@gmail.com>
2024-06-28 14:46:19 +0200

    chore: added aria-description to overlay

--- components/homepage/overlay/Overlay.tsx
+++ components/homepage/overlay/Overlay.tsx
@@ -4,6 +4,9 @@ interface Props {}
 
 export const Overlay: React.FC<Props> = () => {
     return (
-        <div className="w-full h-[36rem] bg-red-600 absolute overlay__gradient" />
+        <div
+            className="w-full h-[36rem] bg-red-600 absolute overlay__gradient"
+            aria-description="Overlay just for the website to look better"
+        />
     );
 };

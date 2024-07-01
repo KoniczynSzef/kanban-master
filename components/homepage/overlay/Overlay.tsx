aaa213b6b48fd8306e5da91b527e4eef4fd9009e
Koniczyn <piotr12konczyk27@gmail.com>
2024-07-01 14:09:28 +0200

    fix: removed overlay from dashboard

--- components/homepage/overlay/Overlay.tsx
+++ components/homepage/overlay/Overlay.tsx
@@ -1,8 +1,19 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import React from "react";
 
 interface Props {}
 
 export const Overlay: React.FC<Props> = () => {
+    const path = usePathname();
+
+    console.log(path);
+
+    if (path === "/dashboard") {
+        return null;
+    }
+
     return (
         <div
             className="w-full h-[36rem] bg-red-600 absolute overlay__gradient"

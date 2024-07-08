103f4492e4f3dba8492c361552ef59f712a7aa2a
KoniczynSzef <koniczynszef@gmail.com>
2024-07-08 10:45:57 +0200

    style: added animation to AnimatedWrapper.tsx so now children pages are animated

--- components/dashboard/AnimatedWrapper.tsx
+++ components/dashboard/AnimatedWrapper.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { SidebarContext } from "@/context/sidebar/sidebar-context";
 import { motion } from "framer-motion";
 import React from "react";
 
@@ -8,11 +9,23 @@ interface Props {
 }
 
 export const AnimatedWrapper: React.FC<Props> = (props) => {
+    const { isExpanded } = React.useContext(SidebarContext);
+
     return (
         <motion.div
-            className="ml-[24rem] mr-[8rem] w-full"
-            initial={{ y: -25, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
+            className="w-full"
+            initial={{
+                y: -25,
+                opacity: 0,
+                marginLeft: "24rem",
+                marginRight: "8rem",
+            }}
+            animate={{
+                y: 0,
+                opacity: 1,
+                marginLeft: isExpanded ? "24rem" : "8rem",
+                marginRight: isExpanded ? "8rem" : "8rem",
+            }}
         >
             {props.children}
         </motion.div>
